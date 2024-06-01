3621d0ab3c2d625365c8567f6c988113dc09fd95
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-01 17:43:07 +0530

    register productline

--- drfcecom/drfcecom/product/admin.py
+++ drfcecom/drfcecom/product/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Brand, Product, Category
+from .models import Brand, Product, Category, ProductLine
 
 admin.site.register(Product)
 admin.site.register(Brand)
 admin.site.register(Category)
+admin.site.register(ProductLine)
