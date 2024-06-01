2da1636e972984e807d59b6496012edd628f5175
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-01 17:42:27 +0530

    add model productline

--- drfcecom/drfcecom/product/models.py
+++ drfcecom/drfcecom/product/models.py
@@ -39,3 +39,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProductLine(models.Model):
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    sku = models.CharField(max_length=100)
+    stock_qty = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.sku
