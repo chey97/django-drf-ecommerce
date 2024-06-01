ade471c248305d8f65627ba9d71de2c07379dbfd
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-01 06:49:39 +0530

    update with schema settings

--- drfcecom/drfcecom/settings/base.py
+++ drfcecom/drfcecom/settings/base.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # External Packages
     "rest_framework",
+    "drf_spectacular",
     # Internal Apps
     "drfcecom.product",
 ]
@@ -104,4 +105,10 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-RESR_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Django DRF Ecommerce",
+}
